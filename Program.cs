361b906b2499a93c7c8f7c1ea212b6a8702c92e6
Bruno Brasolin <50347981+BrunoBrasolin@users.noreply.github.com>
2024-02-25 10:29:08 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -21,7 +21,7 @@ builder.Services.AddScoped(x => new OracleConnection(connectionString));
 builder.Services.AddHealthChecks().AddOracle(connectionString);
 
 WebApplication app = builder.Build();
-app.UseCors(builder => builder.WithOrigins("http://contas.gamidas.dev.br", "http://bills-calculate-percentage.gamidas.dev.br").AllowAnyHeader().AllowAnyMethod());
+app.UseCors(builder => builder.WithOrigins("http://contas.gamidas.dev.br", "http://bills-calculate-percentage.gamidas.dev.br", "http://bills.gamidas.dev.br").AllowAnyHeader().AllowAnyMethod());
 app.UseSwagger();
 app.UseSwaggerUI();
 
